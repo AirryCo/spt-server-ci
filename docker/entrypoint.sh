5425a45394a0385b62454e32a1fa0562a5692e12
AirryCo <herui@stblog.com.cn>
2025-01-14 00:02:22 +0800

    docker: fix the ENV not in effect in regular expression.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -36,11 +36,11 @@ else
   PINGDELAYMS=$webSocketPingDelayMs
 fi
 
-sed -Ei 's/"ip": "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",/"ip": "0.0.0.0",/g' SPT_Data/Server/configs/http.json
-sed -Ei 's/"port": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/"port": ${PORT},/g' SPT_Data/Server/configs/http.json
-sed -Ei 's/"backendIp": "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",/"backendIp": "${IP}",/g' SPT_Data/Server/configs/http.json
-sed -Ei 's/"backendPort": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/"backendPort": ${PORT},/g' SPT_Data/Server/configs/http.json
-sed -Ei 's/"webSocketPingDelayMs": ([0-9]{1,}),/"webSocketPingDelayMs": ${PINGDELAYMS},/g' SPT_Data/Server/configs/http.json
+sed -Ei "s/\"ip\": \"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\",/\"ip\": \"0.0.0.0\",/g" SPT_Data/Server/configs/http.json
+sed -Ei "s/\"port\": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/\"port\": ${PORT},/g" SPT_Data/Server/configs/http.json
+sed -Ei "s/\"backendIp\": \"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\",/\"backendIp\": \"${IP}\",/g" SPT_Data/Server/configs/http.json
+sed -Ei "s/\"backendPort\": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/\"backendPort\": ${PORT},/g" SPT_Data/Server/configs/http.json
+sed -Ei "s/\"webSocketPingDelayMs\": ([0-9]{1,}),/\"webSocketPingDelayMs\": ${PINGDELAYMS},/g" SPT_Data/Server/configs/http.json
 
 chmod +x SPT.Server && ./SPT.Server
 
