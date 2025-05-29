0460721bdfaa53ec0991d7643cbbcc56203664c1
AirryCo <herui@stblog.com.cn>
2025-05-29 18:21:26 +0800

    spt4.0.0(eft0.16.6.0.36679)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -f /opt/spt-server/SPT.Server ]; then
-  appHash=$(md5sum /app/spt-server/SPT.Server | awk '{ print $1 }')
-  exeHash=$(md5sum /opt/spt-server/SPT.Server | awk '{ print $1 }')
+if [ -f /opt/spt-server/SPTarkov.Server ]; then
+  appHash=$(md5sum /app/spt-server/SPTarkov.Server | awk '{ print $1 }')
+  exeHash=$(md5sum /opt/spt-server/SPTarkov.Server | awk '{ print $1 }')
   if [ "$appHash" = "$exeHash" ]; then
     echo "MD5 verification successful!"
   else
@@ -18,10 +18,16 @@ fi
 
 cd /opt/spt-server
 
-if [ -z "$backendIp" ]; then
-  IP=$(hostname -I | awk '{print $1}')
+if [ -z "$ip" ]; then
+  IP="0.0.0.0"
 else
-  IP=$backendIp
+  IP=$ip
+fi
+
+if [ -z "$backendIp" ]; then
+  BACKEND_IP=$(hostname -I | awk '{print $1}')
+else
+  BACKEND_IP=$backendIp
 fi
 
 if [ -z "$backendPort" ]; then
@@ -36,11 +42,10 @@ else
   PINGDELAYMS=$webSocketPingDelayMs
 fi
 
-sed -Ei "s/\"ip\": \"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\",/\"ip\": \"0.0.0.0\",/g" SPT_Data/Server/configs/http.json
-sed -Ei "s/\"port\": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/\"port\": ${PORT},/g" SPT_Data/Server/configs/http.json
-sed -Ei "s/\"backendIp\": \"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\",/\"backendIp\": \"${IP}\",/g" SPT_Data/Server/configs/http.json
-sed -Ei "s/\"backendPort\": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/\"backendPort\": ${PORT},/g" SPT_Data/Server/configs/http.json
-sed -Ei "s/\"webSocketPingDelayMs\": ([0-9]{1,}),/\"webSocketPingDelayMs\": ${PINGDELAYMS},/g" SPT_Data/Server/configs/http.json
+sed -Ei "s/\"ip\": \"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\",/\"ip\": \"${IP}\",/g" Assets/configs/http.json
+sed -Ei "s/\"port\": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/\"port\": ${PORT},/g" Assets/configs/http.json
+sed -Ei "s/\"backendIp\": \"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\",/\"backendIp\": \"${BACKEND_IP}\",/g" Assets/configs/http.json
+sed -Ei "s/\"backendPort\": ([0-9]|[1-9][1-9]{1,3}|[1-5][0-9]{4}|6[1-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]),/\"backendPort\": ${PORT},/g" Assets/configs/http.json
+sed -Ei "s/\"webSocketPingDelayMs\": ([0-9]{1,}),/\"webSocketPingDelayMs\": ${PINGDELAYMS},/g" Assets/configs/http.json
 
-chmod +x SPT.Server && ./SPT.Server
-
+chmod +x SPTarkov.Server && ./SPTarkov.Server
